005cf5d214a44990908adcff8ac0787618889f95
duduskaaa <ergalikhatep@gmail.com>
2025-05-13 10:37:04 +0500

    Update gstage.sh

--- gstage.sh
+++ gstage.sh
@@ -3,6 +3,8 @@
 set -e
 cd /mnt/gentoo
 
-echo "[+] INSTALL & UNTAR STAGE PACKAGE"
+echo "[+] INSTALL STAGE PACKAGE"
 wget https://distfiles.gentoo.org/releases/amd64/autobuilds/20250504T164008Z/stage3-amd64-openrc-20250504T164008Z.tar.xz
+
+echo "[+] UNTAR STAGE PACKAGE"
 tar xpvf stage3-amd64-openrc-202504T164008Z.tar.xz --xattrs-include='*.*' --numeric-owner
