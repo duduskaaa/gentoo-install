5bcd293caac48b4494f18de199bb704ebc6f6a5f
duduskaaa <ergalikhatep@gmail.com>
2025-05-13 10:34:09 +0500

    Update gmount.sh

--- gmount.sh
+++ gmount.sh
@@ -5,15 +5,16 @@ set -e
 echo "[+] MAKE /MNT/GENTOO"
 mkdir -p /mnt/gentoo
 
-echo "[+]MOUNT ROOT"
+echo "[+] MOUNT ROOT"
 mount /dev/nvme0n1p2 /mnt/gentoo
 
+echo "[+] MAKE BOOT & HOME"
 mkdir -p /mnt/boot/efi
 mkdir -p /mnt/home
 
-echo "[+]MOUNT EFI"
+echo "[+] MOUNT EFI"
 mount /dev/nvme0n1p1 /mnt/boot/efi
 
-echo "[+]MOUNT HOME"
+echo "[+] MOUNT HOME"
 mount /dev/nvme0n1p3 /mnt/home
 
