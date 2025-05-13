953dc3610e98891d7feefaee32931a236b7bf539
duduskaaa <ergalikhatep@gmail.com>
2025-05-13 04:49:09 +0000

    Update gfirmware.sh

--- gfirmware.sh
+++ gfirmware.sh
@@ -3,9 +3,7 @@
 set -e
 
 echo "[+] INSTALL LINUX FIRMWARE"
-# Установка пакета с прошивками
 emerge --ask sys-kernel/linux-firmware
 
 echo "[+] CHECKING FOR FIRMWARE"
-# Проверка наличия прошивок в системе
 dmesg | grep firmware
